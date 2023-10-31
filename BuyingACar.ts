06c7e1297da3e83aa960cee6ccfaedaab9f40968
Vidit Shah <vidit0210@protonmail.com>
2023-10-31 20:07:49 +0530

    BuyingACar

---
+++ BuyingACar.ts
@@ -0,0 +1,31 @@
+function nbMonths(
+  startPriceOld: number,
+  startPriceNew: number,
+  savingPerMonth: number,
+  percentLossByMonth: number
+): [number, number] {
+  let months = 0;
+  let savings = 0;
+  let currentPriceOld = startPriceOld;
+  let currentPriceNew = startPriceNew;
+
+  while (currentPriceOld + savings < currentPriceNew) {
+    console.log({ months, savings, currentPriceOld, currentPriceNew });
+    months++;
+    if (months % 2 === 0) {
+      percentLossByMonth += 0.5;
+    }
+
+    currentPriceOld *= 1 - percentLossByMonth / 100;
+    console.log({ currentPriceOld });
+    currentPriceNew *= 1 - percentLossByMonth / 100;
+    console.log({ currentPriceNew });
+    savings += savingPerMonth;
+  }
+
+  return [months, Math.round(currentPriceOld + savings - currentPriceNew)];
+}
+
+// Test
+const result = nbMonths(2000, 8000, 1000, 1.5);
+console.log(result); // Expected output: [6, 766]
