89db0542d5bc9464f699ac7e851043e6fbf5a70e
Vidit Shah <vidit0210@protonmail.com>
2024-04-23 22:25:45 +0530

    Find Uinque

--- findUnique.ts
+++ findUnique.ts
@@ -1,4 +1,4 @@
-export function findUniq(arr: string[]): string {
+export function findUniqOG(arr: string[]): string {
   let newArr = arr.map((a) => [...new Set(a.toLowerCase())].sort().join(""));
   console.log({ newArr });
   return arr.find((str, i) => {
@@ -7,4 +7,31 @@ export function findUniq(arr: string[]): string {
   })!;
 }
 
-console.log(findUniq(["Aa", "aaa", "aaaaa", "BbBb", "Aaaa", "AaAaAa", "a"]));
+export function findUniq(arr: string[]): string {
+  let newArr = arr.map((a) => {
+    console.log("Original string:", a);
+
+    const lowercaseStr = a.toLowerCase();
+    console.log("Lowercase string:", lowercaseStr);
+
+    const uniqueChars = [...new Set(lowercaseStr)];
+    console.log("Unique characters:", uniqueChars);
+
+    const sortedChars = uniqueChars.sort();
+    console.log("Sorted characters:", sortedChars);
+
+    const transformedStr = sortedChars.join("");
+    console.log("Transformed string:", transformedStr);
+
+    return transformedStr;
+  });
+
+  console.log({ newArr });
+
+  return arr.find((str, i) => {
+    console.log(newArr[i]);
+    console.log(newArr.indexOf(newArr[i]), newArr.lastIndexOf(newArr[i]));
+    return newArr.indexOf(newArr[i]) === newArr.lastIndexOf(newArr[i]);
+  })!;
+}
+console.log(findUniq(["silvia", "vasili", "victor"]));
